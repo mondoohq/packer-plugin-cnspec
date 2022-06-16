af684146e0d94ed089bc7a53bc52e63a596c4300
Preslav Gerchev <preslav@mondoo.com>
2022-06-16 22:57:35 +0200

    🐛 Fix SshPrivateKey json naming and extend tests. (#7)
    Signed-off-by: Preslav <preslav@mondoo.com>

--- provisioner/buildinfo_test.go
+++ provisioner/buildinfo_test.go
@@ -11,10 +11,22 @@ import (
 func TestBuildInfo(t *testing.T) {
 
 	data := `
-		{"ConnType":"ssh","Host":"127.0.0.1","ID":"packer-alpine-1599666417","PackerHTTPAddr":"10.0.2.2:8644","PackerHTTPIP":"10.0.2.2","PackerHTTPPort":"8644","PackerRunUUID":"6ef27e2e-f4ea-be8e-7dc9-b1089998c001","Password":"vagrant","Port":2463,"SSHAgentAuth":false,"SSHPrivateKey":"","SSHPrivateKeyFile":"","SSHPublicKey":"","User":"vagrant","WinRMPassword":""}
+		{"ConnType":"ssh","Host":"127.0.0.1","ID":"packer-alpine-1599666417","PackerHTTPAddr":"10.0.2.2:8644","PackerHTTPIP":"10.0.2.2","PackerHTTPPort":"8644","PackerRunUUID":"6ef27e2e-f4ea-be8e-7dc9-b1089998c001","Password":"vagrant","Port":2463,"SSHAgentAuth":false,"SSHPrivateKey":"priv-key","SSHPublicKey":"pub-key","User":"vagrant","WinRMPassword":""}
 	`
 	var buildInfo BuildInfo
 	err := json.Unmarshal([]byte(data), &buildInfo)
 	require.NoError(t, err)
 	assert.Equal(t, "packer-alpine-1599666417", buildInfo.ID)
+	assert.Equal(t, "ssh", buildInfo.ConnType)
+	assert.Equal(t, "127.0.0.1", buildInfo.Host)
+	assert.Equal(t, 2463, buildInfo.Port)
+	assert.Equal(t, "vagrant", buildInfo.User)
+	assert.Equal(t, "vagrant", buildInfo.Password)
+	assert.Equal(t, "10.0.2.2:8644", buildInfo.PackerHTTPAddr)
+	assert.Equal(t, "10.0.2.2", buildInfo.PackerHTTPIP)
+	assert.Equal(t, "8644", buildInfo.PackerHTTPPort)
+	assert.Equal(t, "6ef27e2e-f4ea-be8e-7dc9-b1089998c001", buildInfo.PackerRunUUID)
+	assert.False(t, buildInfo.SSHAgentAuth)
+	assert.Equal(t, "priv-key", buildInfo.SSHPrivateKey)
+	assert.Equal(t, "pub-key", buildInfo.SSHPublicKey)
 }
