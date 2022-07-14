c65235e8a9cd09cec35ee40b05cff3d226e49847
Christoph Hartmann <chris@lollyrock.com>
2022-07-14 15:56:36 +0200

    ⭐️ add platform detection to download script and support macos
    Signed-off-by: Christoph Hartmann <chris@lollyrock.com>

--- download.sh
+++ download.sh
@@ -7,8 +7,29 @@ set +x
 set -e
 set -o pipefail
 
-os=linux
-arch=amd64
+os=""
+case "$(uname -s)" in
+	Linux)  os="linux" ;;
+	Darwin) os="darwin" ;;
+	DragonFly) os="dragonfly" ;;
+	GNU/kFreeBSD) os="freebsd" ;;
+	FreeBSD) os="freebsd" ;;
+	OpenBSD) os="openbsd" ;;
+	SunOS) os="solaris" ;;
+	NetBSD) os="netbsd" ;;
+	*)      fail "Cannot detect OS" ;;
+esac
+
+arch=""
+case "$(uname -m)" in
+	x86_64)  arch="amd64" ;;
+	i386)    arch="386" ;;
+	i686)    arch="386" ;;
+	arm)     arch="arm" ;;
+	aarch64) arch="arm64";;
+	arm64)   arch="arm64";;
+	*)       fail "Cannot detect architecture" ;;
+esac
 
 # automatic download of latest version
 version=$(curl https://api.github.com/repos/mondoohq/packer-plugin-mondoo/releases/latest | jq -r .name)
@@ -21,14 +42,21 @@ echo Download "${archive}" from
 url="https://github.com/mondoohq/packer-plugin-mondoo/releases/download/${version}"
 echo "${url}"
 
-curl -sSL "${url}/${archive}" > ${archive}
-curl -sSL "${url}/${sha}" > ${sha}
+curl -sSL "${url}/${archive}" > "${archive}"
+curl -sSL "${url}/${sha}" > "${sha}"
 
-echo "Validating checksum..."
-cat ${sha} | grep ${archive} | sha256sum -c
+# determine sha tool based on os
+if [ $os = "darwin" ]; then
+  sha256bin='shasum -a 256 -c'
+else
+  sha256bin='sha256sum -c'
+fi
 
-unzip ${archive}
-rm ${archive} ${sha}
+echo "Validating checksum ${sha} ..."
+cat ${sha} | grep ${archive} | ${sha256bin}
+
+unzip "${archive}"
+rm "${archive}" "${sha}"
 
 mkdir -p ~/.packer.d/plugins
 mv "packer-plugin-mondoo_${version}_x5.0_${os}_${arch}" ~/.packer.d/plugins/packer-plugin-mondoo
